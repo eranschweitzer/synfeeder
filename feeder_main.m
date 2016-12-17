261a16633a1b95f3c07d81335f45e197e62f003b
Eran Schweitzer <eranschweitzer@gmail.com>
2016-12-17 14:33:10 -0700

    Updates to input and new data saved
    The input_2.csv file was recreated to insure that all feeders
    have more than 1 node. Otherwise the algorithm appears to be
    stuck in an infinit loop.
    
    pmax_coeff.mat is added which contains the coefficients for a
    power law curve to limit the amount of load assigned to a node
    at a given hop distance.

--- feeder_main.m
+++ feeder_main.m
@@ -11,6 +11,7 @@ for fid = 1:size(in_vars,1)
     N = in_vars.n(fid);
     Stotal = in_vars.mva(fid);
     Pinj_total = in_vars.pinj(fid);
+    fprintf('Processing feeder %d of %d with %d nodes\n',fid,size(in_vars,1),N)
     [n,e] = single_feeder_gen(N,Stotal,Pinj_total);
     n.fid = fid*ones(length(n.id),1);
     e.fid = fid*ones(length(e.id),1); 
@@ -22,4 +23,10 @@ for fid = 1:size(in_vars,1)
         e_tot = table2struct([struct2table(e_tot);struct2table(e)],'ToScalar',true);
     end
 end
-[KL,T_hist,T_fit] = feeder_testing(n_tot,e_tot,'fig',1);
+[KL,T_hist,T_fit] = feeder_testing(n_tot,e_tot,'fig',0);
+
+save('/sine/tmp/eran/radial_feeder_results/input_2_results.mat','n_tot','e_tot','v-7.3')
+%%
+writetable(struct2tab_with_pad(KL),'~/Dropbox/ASU/SINE/Data/feeder_analysis/input_2_KL.csv');
+writetable(struct2tab_with_pad(T_hist),'~/Dropbox/ASU/SINE/Data/feeder_analysis/input_2_T_hist.csv');
+writetable(struct2tab_with_pad(T_fit),'~/Dropbox/ASU/SINE/Data/feeder_analysis/input_2_T_fit.csv');
